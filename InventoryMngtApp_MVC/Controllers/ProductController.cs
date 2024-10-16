c9078cc8ba2cd69359b58ee921e2fcde0fa80672
mary jisan <maryjisna0103@gmail.com>
2024-10-16 21:03:10 +0530

    Add project files.

---
+++ InventoryMngtApp_MVC/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using InventoryMngtApp_MVC.Models.Repository;
+using InventoryMngtApp_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryMngtApp_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository repository = new ProductRepository();
+
+        // Action to show all products
+        public ActionResult Index()
+        {
+            var products = repository.GetAllProducts();
+            return View(products);
+        }
+
+        // Action to create a new product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Action to save the new product
+        [HttpPost]
+        public ActionResult Create(Product product, HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string path = Path.Combine(Server.MapPath("~/Images"), fileName);
+                file.SaveAs(path);
+                product.ProductImage = fileName;
+            }
+
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+
+}
---
+++ InventoryMngtApp_MVC/Models/Repository/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace InventoryMngtApp_MVC.Models.Repository
+{
+    public class ProductRepository 
+    {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["InventoryDB"].ConnectionString;
+
+        public List<Product> GetAllProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("[GetAllProducts]", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Product product = new Product
+                    {
+                        ProductId = Convert.ToInt32(reader["ProductId"]),
+                        ProductName = reader["ProductName"].ToString(),
+                        ProductImage = reader["ProductImage"].ToString(),
+                        Price = Convert.ToDecimal(reader["Price"])
+                    };
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_InsertOrUpdateProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductId", product.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                cmd.Parameters.AddWithValue("@ProductImage", product.ProductImage);
+                cmd.Parameters.AddWithValue("@Price", product.Price);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+}
